5685021c6a1ecae570a98b3c02b44912054321b1
NailMunirov27 <93990535+NailMunirov27@users.noreply.github.com>
2021-11-30 22:53:41 +0500

    fake dir
    fail

--- lesson-2/3_lesson_1_task.py
+++
@@ -1,9 +0,0 @@
-rus_numb = {'zero': 'ноль', 'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять',
-            'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
-
-
-def num_translate(eng):
-    return rus_numb.get(eng)
-
-
-print(num_translate(input('')))
--- lesson-2/3_lesson_2_task.py
+++
@@ -1,4 +0,0 @@
-# for noun, adv, adj in zip(nouns, adverbs, adjectives):
-#     rand12 = randrange(len(nouns))
-#     joke = (f"{rand12[noun]} {adv} {adj}, {noun} {adv} {adj}. ")
-# return joke * n
