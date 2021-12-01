27c459198c29f7b6180ce56cbf54b33d35e3b026
Nail <nailmunirov@yandex.ru>
2021-12-01 18:47:30 +0500

    homework lesson3

--- lesson-3/3_lesson_3_task.py
+++ lesson-3/3_lesson_3_task.py
@@ -1,32 +1,15 @@
-from random import randrange, choice
-
-nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
-adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
-adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
-
-
-def get_jokes(r, repeat=False):
-
-    """
-    Генератор случайных шуток из имеющихся списков.
-
-    :param r: количество шуток
-    :param repeat: наличие уникальных шуток
-    :return: список сгенерированных шуток
-
-    """
-
-    joke_rand = []
-    list_min = min(nouns, adverbs, adjectives)
-
-    while r and len(list_min):
-        num = randrange(len(list_min))
-        if repeat:
-            joke_rand.append(f"{nouns.pop(num)} {adverbs.pop(num)} {adjectives.pop(num)}")
+def thesaurus(*args):
+    names = {}
+    for name in args:
+        N = name[0]
+        if N in names:
+            names[N] += [name]
         else:
-            joke_rand.append(f"{choice(nouns)} {choice(adverbs)} {choice(adjectives)}")
-        r -= 1
+            names[N] = [name]
+    return names.items()
 
-    return joke_rand
+print(thesaurus("Иван", "Мария", "Петр", "Илья"))
 
-print(get_jokes(4, True))
+
+
+
